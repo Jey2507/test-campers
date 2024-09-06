ffb9702bc9557cbd314e3195b5954c29e5fff015
Oleh Toporovych <otoporovych2@gmail.com>
2024-09-06 23:56:27 +0200

    Update TrackReviews.jsx

--- src/components/TrackReviews/TrackReviews.jsx
+++ src/components/TrackReviews/TrackReviews.jsx
@@ -1,10 +1,20 @@
 
-// import css from "../SearchBar/SearchBar.module.css"
+import css from "../SearchBar/SearchBar.module.css"
+
+import { useSelector } from "react-redux";
+import { selectTracks } from "../../redux/track/selectors";
 
 export default function TrackReviews() {
+    const { currentItem } = useSelector(selectTracks);
     return (
         <>
-        
+            <ul>
+                {currentItem.reviews.map((item) => 
+                    <li id={item.reviewer_name}>
+                        {comment}
+                    </li>
+                )}
+            </ul>
         </>
     )
 }
